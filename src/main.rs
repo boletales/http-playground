81971bacdfd6aba570dca0fe21c62753ab4e0149
boletales <30789006+boletales@users.noreply.github.com>
2025-05-26 23:59:22 +0900

    hyperのバージョンを変更

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-use hyper::{Body, Request, Response, Server, Method, StatusCode};
-use hyper::service::{make_service_fn, service_fn};
 use serde::{Deserialize, Serialize};
-use std::convert::Infallible;
-use std::sync::{Arc, Mutex};
+use std::{
+    convert::Infallible,
+    sync::{Arc, Mutex},
+};
+use hyper::{
+    server::conn::http1,
+    service::service_fn,
+    body::{Bytes, Incoming},
+    {Request, Response, Method, StatusCode},
+};
+use hyper_util::rt::TokioIo;
+use http_body_util::{BodyExt, Full};
+use tokio::net::TcpListener;
+use std::net::SocketAddr;
 
 #[derive(Serialize, Deserialize, Clone)]
 struct Count {
@@ -11,35 +21,34 @@ struct Count {
 
 type SharedCount = Arc<Mutex<Count>>;
 
-async fn handle_request(req: Request<Body>, count: SharedCount) -> Result<Response<Body>, Infallible> {
-    let path = req.uri().path();
-    match (req.method(), path) {
+async fn handle_request(req: Request<Incoming>, count: SharedCount) -> Result<Response<Full<Bytes>>, Infallible> {
+    match (req.method(), req.uri().path()) {
         // クライアントページ
         (&Method::GET, "/") => {
             let html = include_str!("../static/index.html");
-            Ok(Response::new(Body::from(html)))
+            Ok(Response::new(Full::new(Bytes::from(html))))
         }
         // カウント取得
         (&Method::GET, "/getcount") => {
             let count = count.lock().unwrap().clone();
             let body = serde_json::to_string(&count).unwrap();
-            Ok(Response::new(Body::from(body)))
+            Ok(Response::new(Full::new(Bytes::from(body))))
         }
         // カウント加算
         (&Method::POST, "/addcount") => {
-            let whole_body = hyper::body::to_bytes(req.into_body()).await.unwrap();
-            let add: Count = serde_json::from_slice(&whole_body).unwrap_or(Count { value: 1 });
+            let whole_body = req.into_body().collect().await.unwrap().to_bytes();
+            let add: Count = serde_json::from_slice(&whole_body).unwrap_or(Count { value: 0 });
             let mut count = count.lock().unwrap();
             count.value += add.value;
             let body = serde_json::to_string(&*count).unwrap();
-            Ok(Response::new(Body::from(body)))
+            Ok(Response::new(Full::new(Bytes::from(body))))
         }
         // カウントリセット
         (&Method::POST, "/resetcount") => {
             let mut count = count.lock().unwrap();
             count.value = 0;
             let body = serde_json::to_string(&*count).unwrap();
-            Ok(Response::new(Body::from(body)))
+            Ok(Response::new(Full::new(Bytes::from(body))))
         }
         _ => {
             let mut not_found = Response::default();
@@ -50,20 +59,34 @@ async fn handle_request(req: Request<Body>, count: SharedCount) -> Result<Respon
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    const PORT: u16 = 3000;
+    let addr = SocketAddr::from(([127, 0, 0, 1], PORT));
+    // We create a TcpListener and bind it to 127.0.0.1:3000
+    let listener = TcpListener::bind(addr).await?;
+    
+    println!("listening on http://{addr}");
+    
     let count = Arc::new(Mutex::new(Count { value: 0 }));
-    let make_svc = make_service_fn(move |_| {
+    // We start a loop to continuously accept incoming connections
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement
+        // `hyper::rt` IO traits.
+        let io = TokioIo::new(stream);
+
         let count = count.clone();
-        async move {
-            Ok::<_, Infallible>(service_fn(move |req| {
-                handle_request(req, count.clone())
-            }))
-        }
-    });
-    let addr = ([127, 0, 0, 1], 8080).into();
-    let server = Server::bind(&addr).serve(make_svc);
-    println!("Listening on http://{}", addr);
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            // Finally, we bind the incoming connection to our `hello` service
+            if let Err(err) = http1::Builder::new()
+                // `service_fn` converts our function in a `Service`
+                .serve_connection(io,service_fn(move |req| handle_request(req, count.clone())))
+                .await
+            {
+                eprintln!("Error serving connection: {:?}", err);
+            }
+        });
     }
 }
