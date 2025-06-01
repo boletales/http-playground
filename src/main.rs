e117d97b4e270edf7944cc7476c67704da3fdb91
boletales <30789006+boletales@users.noreply.github.com>
2025-06-01 20:45:13 +0900

    add API document

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
+use http_body_util::{BodyExt, Full};
+use hyper::{
+    body::{Bytes, Incoming},
+    server::conn::http1,
+    service::service_fn,
+    {Method, Request, Response, StatusCode},
+};
+use hyper_util::rt::TokioIo;
 use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
 use std::{
     convert::Infallible,
     sync::{Arc, Mutex},
 };
-use hyper::{
-    server::conn::http1,
-    service::service_fn,
-    body::{Bytes, Incoming},
-    {Request, Response, Method, StatusCode},
-};
-use hyper_util::rt::TokioIo;
-use http_body_util::{BodyExt, Full};
 use tokio::net::TcpListener;
-use std::net::SocketAddr;
 
 #[derive(Serialize, Deserialize, Clone)]
 struct Count {
@@ -21,7 +21,10 @@ struct Count {
 
 type SharedCount = Arc<Mutex<Count>>;
 
-async fn handle_request(req: Request<Incoming>, count: SharedCount) -> Result<Response<Full<Bytes>>, Infallible> {
+async fn handle_request(
+    req: Request<Incoming>,
+    count: SharedCount,
+) -> Result<Response<Full<Bytes>>, Infallible> {
     let method = req.method().clone();
     let path = req.uri().path().to_owned();
     println!("\n\n\nRequest Received!");
@@ -32,7 +35,7 @@ async fn handle_request(req: Request<Incoming>, count: SharedCount) -> Result<Re
     }
     let whole_body = req.into_body().collect().await.unwrap().to_bytes();
     println!("* Body: {:?}", String::from_utf8_lossy(&whole_body));
-    
+
     let res = match (&method, path.as_str()) {
         // クライアントページ
         (&Method::GET, "/") => {
@@ -56,30 +59,29 @@ async fn handle_request(req: Request<Incoming>, count: SharedCount) -> Result<Re
             res
         }
         // カウント加算
-        (&Method::POST, "/addcount") => {
-            match serde_json::from_slice::<Count>(&whole_body) {
-                Ok(add) => {
-                    let mut count = count.lock().unwrap();
-                    count.value += add.value;
-                    let body = serde_json::to_string(&*count).unwrap();
-                    let mut res = Response::new(Full::new(Bytes::from(body)));
-                    res.headers_mut().insert(
-                        hyper::header::CONTENT_TYPE,
-                        hyper::header::HeaderValue::from_static("application/json"),
-                    );
-                    res
-                },
-                Err(_) => {
-                    let mut res: Response<Full<Bytes>> = Response::new(Full::new(Bytes::from("Bad Request (Invalid JSON)")));
-                    *res.status_mut() = StatusCode::BAD_REQUEST;
-                    res.headers_mut().insert(
-                        hyper::header::CONTENT_TYPE,
-                        hyper::header::HeaderValue::from_static("text/plain"),
-                    );
-                    res
-                }
+        (&Method::POST, "/addcount") => match serde_json::from_slice::<Count>(&whole_body) {
+            Ok(add) => {
+                let mut count = count.lock().unwrap();
+                count.value += add.value;
+                let body = serde_json::to_string(&*count).unwrap();
+                let mut res = Response::new(Full::new(Bytes::from(body)));
+                res.headers_mut().insert(
+                    hyper::header::CONTENT_TYPE,
+                    hyper::header::HeaderValue::from_static("application/json"),
+                );
+                res
             }
-        }
+            Err(_) => {
+                let mut res: Response<Full<Bytes>> =
+                    Response::new(Full::new(Bytes::from("Bad Request (Invalid JSON)")));
+                *res.status_mut() = StatusCode::BAD_REQUEST;
+                res.headers_mut().insert(
+                    hyper::header::CONTENT_TYPE,
+                    hyper::header::HeaderValue::from_static("text/plain"),
+                );
+                res
+            }
+        },
         // カウントリセット
         (&Method::POST, "/resetcount") => {
             let mut count = count.lock().unwrap();
@@ -109,7 +111,18 @@ async fn handle_request(req: Request<Incoming>, count: SharedCount) -> Result<Re
     for (k, v) in res.headers() {
         println!("    * {}: {:?}", k, v);
     }
-    println!("* Body: {:?}", String::from_utf8_lossy(res.body().clone().collect().await.unwrap().to_bytes().as_ref()));
+    println!(
+        "* Body: {:?}",
+        String::from_utf8_lossy(
+            res.body()
+                .clone()
+                .collect()
+                .await
+                .unwrap()
+                .to_bytes()
+                .as_ref()
+        )
+    );
     Ok(res)
 }
 
@@ -119,9 +132,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = SocketAddr::from(([127, 0, 0, 1], PORT));
     // We create a TcpListener and bind it to 127.0.0.1:3000
     let listener = TcpListener::bind(addr).await?;
-    
+
     println!("listening on http://{addr}");
-    
+
     let count = Arc::new(Mutex::new(Count { value: 0 }));
     // We start a loop to continuously accept incoming connections
     loop {
@@ -137,7 +150,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             // Finally, we bind the incoming connection to our `hello` service
             if let Err(err) = http1::Builder::new()
                 // `service_fn` converts our function in a `Service`
-                .serve_connection(io,service_fn(move |req| handle_request(req, count.clone())))
+                .serve_connection(
+                    io,
+                    service_fn(move |req| handle_request(req, count.clone())),
+                )
                 .await
             {
                 eprintln!("Error serving connection: {:?}", err);
